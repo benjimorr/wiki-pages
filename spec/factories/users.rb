9cb6d4b3470028df902a82633442fa2fdf54b095
Ben Morrison <benmorrison0@gmail.com>
2017-09-17 14:30:39 -0400

    Checkpoint 2 sign up: added specs and SendGrid

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
     pw = "mypassword"
 
     factory :user do
-        name "Example Name"
         sequence(:email){|n| "user#{n}@factory.com"}
         password pw
         password_confirmation pw
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,37 +3,37 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
     let(:user) { create(:user) }
 
-    # Shoulda tests for name
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_length_of(:name).is_at_least(1) }
-
     # Shoulda tests for email
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to validate_length_of(:email).is_at_least(3) }
-    it { is_expected.to allow_value("user@buzzthread.com").for(:email) }
+    it { is_expected.to allow_value("user@wikipages.com").for(:email) }
 
     # Shoulda tests for password
     it { is_expected.to validate_presence_of(:password) }
-    it { is_expected.to have_secure_password }
     it { is_expected.to validate_length_of(:password).is_at_least(6) }
 
     describe "attributes" do
-        it "should have name and email attributes" do
-            expect(user).to have_attributes(name: user.name, email: user.email)
+        it "should have email attribute" do
+            expect(user).to have_attributes(email: user.email)
         end
     end
 
     describe "invalid user" do
-        let(:user_with_invalid_name) { build(:user, name: "") }
         let(:user_with_invalid_email) { build(:user, email: "") }
-
-        it "should be an invalid user due to blank name" do
-            expect(user_with_invalid_name).to_not be_valid
-        end
 
         it "should be an invalid user due to blank email" do
             expect(user_with_invalid_email).to_not be_valid
+        end
+    end
+
+    describe "email confirmation" do
+        before do
+            @new_user = FactoryGirl.create(:user)
+        end
+
+        it "should send an email confirmation after creation" do
+            expect(Devise.mailer.deliveries.last).to eq(@new_user.email)
         end
     end
 end
